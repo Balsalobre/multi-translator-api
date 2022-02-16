ffaa965781ac6fa255ac7b0c9a4b96c99324e6cb
balsalobre <carlosocb@gmail.com>
2022-02-16 16:06:37 +0100

    implementing translator service

--- src/translator/translator.controller.ts
+++ src/translator/translator.controller.ts
@@ -1,11 +1,20 @@
-import { Controller, HttpStatus, Post, Res } from '@nestjs/common';
+import { Controller, Get, HttpStatus, Post, Res } from '@nestjs/common';
+import { TranslatorService } from './translator.service';
 
 @Controller('translator')
 export class TranslatorController {
+  constructor(private translatorService: TranslatorService) {}
+
   @Post('/create')
-  createTranslation(@Res() res) {
+  async createTranslation(@Res() res) {
     return res.status(HttpStatus.OK).json({
-      message: 'OK',
+      message: 'OK'
     });
+  }
+
+  @Get('/metadata')
+  async getMetadata(@Res() res) {
+    const metadata = await this.translatorService.metadata();
+    return res.status(HttpStatus.OK).json(metadata);
   }
 }
--- src/translator/translator.service.ts
+++ src/translator/translator.service.ts
@@ -1,4 +1,33 @@
 import { Injectable } from '@nestjs/common';
-
+import { google, sheets_v4 } from 'googleapis';
+import { resolve } from 'path';
+import { GoogleAuth } from 'googleapis-common';
 @Injectable()
-export class TranslatorService {}
+export class TranslatorService {
+  googleAuth(): GoogleAuth {
+    const configPath = resolve(__dirname, '..', 'config');
+    return new google.auth.GoogleAuth({
+      keyFile: `${configPath}/google.credentials.json`,
+      scopes: process.env.SCOPES
+    });
+  }
+
+  async googleSheetsClient(): Promise<sheets_v4.Sheets> {
+    const auth: GoogleAuth = await this.googleAuth();
+    const client = await auth.getClient();
+    return google.sheets({ version: 'v4', auth: client });
+  }
+
+  async metadata() {
+    try {
+      const googleSheets = await this.googleSheetsClient();
+      const metaData = await googleSheets.spreadsheets.get({
+        auth: this.googleAuth(),
+        spreadsheetId: process.env.SPREADSHEET_ID
+      });
+      return metaData;
+    } catch (error) {
+      console.error('ERROR GETTING METADATA', error);
+    }
+  }
+}
