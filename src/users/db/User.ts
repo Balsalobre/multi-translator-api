22fb2d4c692de022a0ca8c5ab7ad3ce0ebca3607
balsalobre <carlosocb@gmail.com>
2022-02-17 23:19:40 +0100

    updating user entity

--- src/users/db/User.ts
+++ src/users/db/User.ts
@@ -1,1 +1,23 @@
-export class User {}
+import { AggregateRoot } from '@nestjs/cqrs';
+
+export class User extends AggregateRoot {
+  constructor(
+    private readonly _name: string,
+    private readonly _login: string,
+    private readonly _avatar_url: string
+  ) {
+    super();
+  }
+
+  public get name(): string {
+    return this._name;
+  }
+
+  public get login(): string {
+    return this._login;
+  }
+
+  public get avatar_url(): string {
+    return this._avatar_url;
+  }
+}
