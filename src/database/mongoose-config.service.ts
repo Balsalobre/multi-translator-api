3065650ac829d94011e6b0430c3c3b6d9a73f176
balsalobre <carlosocb@gmail.com>
2022-03-09 17:50:48 +0100

    setting atlas database

--- src/database/mongoose-config.service.ts
+++ src/database/mongoose-config.service.ts
@@ -4,11 +4,7 @@ import { MongooseModuleOptions, MongooseOptionsFactory } from '@nestjs/mongoose'
 @Injectable()
 export class MongooseConfigService implements MongooseOptionsFactory {
   createMongooseOptions(): MongooseModuleOptions {
-    const userAndPass = `${process.env.MONGO_INITDB_ROOT_USERNAME}:${process.env.MONGO_INITDB_ROOT_PASSWORD}`;
-    const url = `${process.env.MONGO_INITDB_URL}`;
-    const port = `${process.env.MONGO_INITDB_PORT}`;
-    const uri = `mongodb://${userAndPass}@${url}:${port}/`;
-
-    return { uri };
+    const mongoAtalasUri = `${process.env.MONGO_ATLAS_URI}`;
+    return { uri: mongoAtalasUri };
   }
 }
